70f49a67e905f4ebb9026cf8edf62c005ba16eb3
Latorredebabel <sebastian-castros@unilibre.edu.co>
2025-03-16 17:19:10 -0500

    Delete src directory

--- src/assets/js/app.js
+++
@@ -1,6 +0,0 @@
-// Este archivo puede contener código común para toda la aplicación
-
-// Ejemplo de código común que podría estar aquí
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Aplicación cargada');
-});
